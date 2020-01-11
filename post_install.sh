be044a8d0e0473298761188442fd9d4ab10113dc
Fredrik Palm <fredrik@thefarm.se>
2020-01-11 19:32:10 +0100

    And another fine yarn thing.. it leaves some files (the .git folder) on updates

--- post_install.sh
+++ post_install.sh
@@ -5,15 +5,13 @@ while IFS= read -r line; do
   if [[ $line =~ (.*)\$\$\$(.*) ]] ; then 
     path=$(echo ${BASH_REMATCH[1]} | sed 's/.*= //')
     url=$(echo ${BASH_REMATCH[2]} | sed 's/.*= //')
+
     if [ "$(ls -A ${path})" ] ; then 
-      echo "Folder ${path} seems to exist, performing git pull"
-      start=$PWD
-      cd $path
-      git pull 
-      cd $PWD 
-    else 
-      echo "Folder ${path} doesn't exist, performing git clone"
-      git clone $url $path
+      echo "Folder ${path} seems to exist, removing the folder"
+      rm -rf $path
     fi 
+      
+    echo "Folder ${path} doesn't exist, performing git clone"
+    git clone $url $path
   fi 
 done
