cb94cf1170c4806d968aa995b73bca8b31e362e2
Fredrik Palm <fredrik@thefarm.se>
2020-01-21 11:39:07 +0100

    Fix example, useTrackPlayerProgress >> useProgress
    * Prettier did some formatting too.

--- example/react/components/Player.js
+++ example/react/components/Player.js
@@ -1,35 +1,28 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import TrackPlayer, {
   State,
   Event,
-  useTrackPlayerProgress,
   usePlaybackState,
-  useTrackPlayerEvents
-} from "react-native-track-player";
-import {
-  Image,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  ViewPropTypes
-} from "react-native";
+  useProgress,
+  useTrackPlayerEvents,
+} from 'react-native-track-player'
+import { Image, StyleSheet, Text, TouchableOpacity, View, ViewPropTypes } from 'react-native'
 
 function ProgressBar() {
-  const progress = useTrackPlayerProgress();
+  const progress = useProgress()
 
   return (
     <View style={styles.progress}>
-      <View style={{ flex: progress.position, backgroundColor: "red" }} />
+      <View style={{ flex: progress.position, backgroundColor: 'red' }} />
       <View
         style={{
           flex: progress.duration - progress.position,
-          backgroundColor: "grey"
+          backgroundColor: 'grey',
         }}
       />
     </View>
-  );
+  )
 }
 
 function ControlButton({ title, onPress }) {
@@ -37,37 +30,34 @@ function ControlButton({ title, onPress }) {
     <TouchableOpacity style={styles.controlButtonContainer} onPress={onPress}>
       <Text style={styles.controlButtonText}>{title}</Text>
     </TouchableOpacity>
-  );
+  )
 }
 
 ControlButton.propTypes = {
   title: PropTypes.string.isRequired,
-  onPress: PropTypes.func.isRequired
-};
+  onPress: PropTypes.func.isRequired,
+}
 
 export default function Player(props) {
-  const playbackState = usePlaybackState();
-  const [trackTitle, setTrackTitle] = useState("");
-  const [trackArtwork, setTrackArtwork] = useState("");
-  const [trackArtist, setTrackArtist] = useState("");
-  useTrackPlayerEvents(["playback-track-changed"], async event => {
+  const playbackState = usePlaybackState()
+  const [trackTitle, setTrackTitle] = useState('')
+  const [trackArtwork, setTrackArtwork] = useState('')
+  const [trackArtist, setTrackArtist] = useState('')
+  useTrackPlayerEvents(['playback-track-changed'], async event => {
     if (event.type === Event.PlaybackTrackChanged) {
-      const track = await TrackPlayer.getTrack(event.nextTrack);
-      setTrackTitle(track.title);
-      setTrackArtist(track.artist);
-      setTrackArtwork(track.artwork);
+      const track = await TrackPlayer.getTrack(event.nextTrack)
+      setTrackTitle(track.title)
+      setTrackArtist(track.artist)
+      setTrackArtwork(track.artwork)
     }
-  });
+  })
 
-  const { style, onNext, onPrevious, onTogglePlayback } = props;
+  const { style, onNext, onPrevious, onTogglePlayback } = props
 
-  var middleButtonText = "Play";
+  var middleButtonText = 'Play'
 
-  if (
-    playbackState === State.Playing ||
-    playbackState === State.Buffering
-  ) {
-    middleButtonText = "Pause";
+  if (playbackState === State.Playing || playbackState === State.Buffering) {
+    middleButtonText = 'Pause'
   }
 
   return (
@@ -77,64 +67,64 @@ export default function Player(props) {
       <Text style={styles.title}>{trackTitle}</Text>
       <Text style={styles.artist}>{trackArtist}</Text>
       <View style={styles.controls}>
-        <ControlButton title={"<<"} onPress={onPrevious} />
+        <ControlButton title={'<<'} onPress={onPrevious} />
         <ControlButton title={middleButtonText} onPress={onTogglePlayback} />
-        <ControlButton title={">>"} onPress={onNext} />
+        <ControlButton title={'>>'} onPress={onNext} />
       </View>
     </View>
-  );
+  )
 }
 
 Player.propTypes = {
   style: ViewPropTypes.style,
   onNext: PropTypes.func.isRequired,
   onPrevious: PropTypes.func.isRequired,
-  onTogglePlayback: PropTypes.func.isRequired
-};
+  onTogglePlayback: PropTypes.func.isRequired,
+}
 
 Player.defaultProps = {
-  style: {}
-};
+  style: {},
+}
 
 const styles = StyleSheet.create({
   card: {
-    width: "80%",
+    width: '80%',
     elevation: 1,
     borderRadius: 4,
     shadowRadius: 2,
     shadowOpacity: 0.1,
-    alignItems: "center",
-    shadowColor: "black",
-    backgroundColor: "white",
-    shadowOffset: { width: 0, height: 1 }
+    alignItems: 'center',
+    shadowColor: 'black',
+    backgroundColor: 'white',
+    shadowOffset: { width: 0, height: 1 },
   },
   cover: {
     width: 140,
     height: 140,
     marginTop: 20,
-    backgroundColor: "grey"
+    backgroundColor: 'grey',
   },
   progress: {
     height: 1,
-    width: "90%",
+    width: '90%',
     marginTop: 10,
-    flexDirection: "row"
+    flexDirection: 'row',
   },
   title: {
-    marginTop: 10
+    marginTop: 10,
   },
   artist: {
-    fontWeight: "bold"
+    fontWeight: 'bold',
   },
   controls: {
     marginVertical: 20,
-    flexDirection: "row"
+    flexDirection: 'row',
   },
   controlButtonContainer: {
-    flex: 1
+    flex: 1,
   },
   controlButtonText: {
     fontSize: 18,
-    textAlign: "center"
-  }
-});
+    textAlign: 'center',
+  },
+})
